c4316e0fe141c94854e124834a2dc43c08a39755
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2024-08-16 15:50:18 +0600

    python practice

---
+++ problem_3.py
@@ -0,0 +1,10 @@
+while True:
+
+ num=int(input())
+ if num==-1:
+   break
+
+ if num%2==0:
+    print("even")
+ else:
+    print("odd")
---
+++ problem_5.py
@@ -0,0 +1,10 @@
+a=int(input())
+if a>=20000:
+    print("Gucci Bag")
+    print("Gucci Belt")
+elif a>=10000:
+    print("Gucci Bag")
+elif a>=5000:
+    print("Levis Bag")
+else:
+    print("Something")
